4fa4eaafc08454e5754fc52e592c94b370dd174a
Liam Griffiths <rockinliam@gmail.com>
2015-04-27 13:02:42 +0100

    Base Project

---
+++ CastleTilt/Assets/Models/Environment/Particles/ParticleAutoDestroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleAutoDestroy : MonoBehaviour {
+
+	private ParticleSystem particles;
+
+
+	public void Start() 
+	{
+		particles = GetComponent<ParticleSystem>();
+	}
+
+	public void Update() 
+	{
+		if(particles)
+		{
+			if(!particles.IsAlive())
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
